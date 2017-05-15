66acce7f0e396dddf57957d963eeacc63080503a
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-15 16:28:48 +0200

    Correction du script install-dhcp-failover.ps1

--- install-dhcp-failover.ps1
+++ install-dhcp-failover.ps1
@@ -1,4 +1,4 @@
 # Installation de DHCP
 Install-WindowsFeature -name DHCP -IncludeManagementTools -IncludeAllSubFeature
 # Mise en place d'un server secondaire de type loadbalancing
-Add-DhcpServerv4Failover -Name "OpenWin" -ScopeId 172.17.2.0,172.17.3.0 -StateSwitchInterval 01:00:00 -PartenerServer "OPENWIN-DHCP01" -AutoStateTransition $true -ServerRole Standby -SharedSecret "csecret" -WhateIf
+Add-DhcpServerv4Failover -Name "OpenWin" -ScopeId 172.17.2.0,172.17.3.0 -StateSwitchInterval 01:00:00 -PartnerServer OPENWIN-DHCP01.alsace.openwin.com -AutoStateTransition $true -ServerRole Standby -SharedSecret "0p3nW1n" -WhatIf
