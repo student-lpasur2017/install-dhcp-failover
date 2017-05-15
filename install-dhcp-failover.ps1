f1b6ecd2fbec644b7eb95228e1061ac4bb38c5d2
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-15 10:37:09 +0200

    Ajout d'un -WhateIf pour tester la configuration

--- install-dhcp-failover.ps1
+++ install-dhcp-failover.ps1
@@ -1,4 +1,4 @@
 # Installation de DHCP
 Install-WindowsFeature -name DHCP -IncludeManagementTools -IncludeAllSubFeature
 # Mise en place d'un server secondaire de type loadbalancing
-Add-DhcpServer4Failover -Name "OpenWin" -PartenerServer "OPENWIN-DHCP01" -AutoStateTransition $true -ServerRole Standby -SharedSecret "csecret"
+Add-DhcpServer4Failover -Name "OpenWin" -PartenerServer "OPENWIN-DHCP01" -AutoStateTransition $true -ServerRole Standby -SharedSecret "csecret" -WhateIf
